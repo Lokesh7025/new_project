5382789ea149aeb9b422eca92ed46e4ebee9f9ea
Lokesh Selvam <lokeshselvam7025@gmail.com>
2025-05-08 11:11:13 +0530

    First commit

---
+++ first.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class object{
+    public:
+    int x;
+    int operator ()(int y){
+        return y*x;
+    }
+};
+int main(){
+    object a(2);
+    for(int i=0;i<5;i++){
+        cout<<a(i)<<endl;
+    }
+    return 0;
+}
